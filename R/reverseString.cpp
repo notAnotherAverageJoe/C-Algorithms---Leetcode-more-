be293323331d9ff8293a19b5eac3008de4485a79
joseph skokan <joeskokan20@gmail.com>
2024-12-26 22:22:50 -0500

    reverse string with swap

---
+++ R/reverseString.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+// first put the return type which will be a string
+//  second put the input variable in.
+std::string reversed(std::string str)
+{
+    // left will be the first character in the string
+    //  right will be the last character in the string
+    int left = 0;
+    int right = str.size() - 1;
+
+    // return the str at the end
+    return str;
+}
+
+int main()
+{
+
+    return 0;
+}
