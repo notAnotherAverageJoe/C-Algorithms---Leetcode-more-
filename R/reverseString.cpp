b8126f5fbfdf6e165fcb37a6f03b2f04a44133b5
joseph skokan <joeskokan20@gmail.com>
2024-12-26 22:31:38 -0500

    rev with method done

--- R/reverseString.cpp
+++ R/reverseString.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <string>
-
-// first put the return type which will be a string
-//  second put the input variable in.
+// reverse string with a the STD library swap
+//  first put the return type which will be a string
+//   second put the input variable in.
 std::string reversed(std::string str)
 {
     // left will be the first character in the string
-    //  right will be the last character in the string
     int left = 0;
+
+    //  right will be the last character in the string
     int right = str.size() - 1;
+    // while str[0] is < EXAMPLE str[10] keep going, once the left pointer and right pointer
+    //  interesct it ends.
+    while (left < right)
+    {
+        std::swap(str[left], str[right]);
+        left++;
+        right--;
+    }
 
     // return the str at the end
     return str;
@@ -16,6 +25,12 @@ std::string reversed(std::string str)
 
 int main()
 {
+    std::string greetings = "Hello,world!";
+    std::cout << "Original: " << greetings << std::endl;
+    std::cout << "Reversed: " << reversed(greetings) << std::endl;
 
     return 0;
 }
+
+// Original: Hello,world!
+// Reversed: !dlrow,olleH
